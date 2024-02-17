6198258ceff4c28c5cbf56cb094a41b76c80bc70
Ramsha <ramshatarannum07@gmail.com>
2024-02-17 13:35:18 +0530

    added rspec and code for single number in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,13 @@
 class StringCalculator
-	def add(x)
-		return 0 if x == ""
+	def add(number)
+		#for emepty string
+		return 0 if number == ""
+
+		special_character = ','
+
+		#to handle single number in string 
+		result = number.split(special_character).map(&:to_i)
+		return result.sum
 	end
 end
+
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,12 @@ describe StringCalculator do
 			expect(calculator.add("")).to eql(0)
 		end
 	end
+
+	context "when string contains single number" do 
+		it "should return the number itself" do 
+			calculator = StringCalculator.new
+			expect(calculator.add("1")).to eql(1)
+			expect(calculator.add("3")).to eql(3)
+		end
+	end
 end
